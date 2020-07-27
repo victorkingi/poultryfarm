249d0e9890e65a821daf6d62be0cf9060125217d
victorkingi <65971651+victorkingi@users.noreply.github.com>
2020-07-27 18:16:02 +0300

    new commit

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,14 +4,14 @@ import 'firebase/auth';
 import 'firebase/functions';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD3tzp-dWkeucSmG8dME_J7Zlxj9vm1hvw",
-    authDomain: "poultry101-6b1ed.firebaseapp.com",
-    databaseURL: "https://poultry101-6b1ed.firebaseio.com",
-    projectId: "poultry101-6b1ed",
-    storageBucket: "poultry101-6b1ed.appspot.com",
-    messagingSenderId: "921557433248",
-    appId: "1:921557433248:web:ea8b85d8f07833da93c2c5",
-    measurementId: "G-FT0FWW0CWR"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 // Initialize Firebase
 
