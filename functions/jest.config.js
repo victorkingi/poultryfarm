05ccbbc88aaadc54b4ee34880b48e13ee9625356
victorkingi <kingivictor77@gmail.com>
2020-11-07 17:19:24 +0000

    changes

--- functions/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    testRegex: 'src(/__tests__/.*|(\\.|/)(__tests__|spec))\\.(jsx?|tsx?)$',
-    testPathIgnorePatterns: ['lib/', 'node_modules/'],
-    moduleFileExtensions: ['js','ts','tsx','jsx','json','node'],
-    testEnvironment: 'node',
-    rootDir: 'src'
-}
