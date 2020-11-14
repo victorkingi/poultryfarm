9a3213cbc65bc5a53c38188f276b52585e0a466a
victorkingi <kingivictor77@gmail.com>
2020-11-14 15:47:38 +0000

    changes

--- src/services/api/firebase configurations/fbConfig.js
+++ src/services/api/firebase configurations/fbConfig.js
@@ -7,14 +7,14 @@ import 'firebase/performance';
 import 'firebase/storage';
 
 let firebaseConfig = {
-    apiKey: "AIzaSyD3tzp-dWkeucSmG8dME_J7Zlxj9vm1hvw",
-    authDomain: "poultry101-6b1ed.firebaseapp.com",
-    databaseURL: "https://poultry101-6b1ed.firebaseio.com",
-    projectId: "poultry101-6b1ed",
-    storageBucket: "poultry101-6b1ed.appspot.com",
-    messagingSenderId: "921557433248",
-    appId: "1:921557433248:web:5828718b6146d8a293c2c5",
-    measurementId: "G-9EHX8Z8LDZ"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId:"",
+    appId: "",
+    measurementId: ""
 };
 // Initialize Firebase
 
@@ -27,7 +27,7 @@ const messaging = firebase.messaging();
 const perf = firebase.performance();
 const storage = firebase.storage();
 
-messaging.usePublicVapidKey("BAKZFaJ-BRXiOD_-PIzqLcqwXOjqx0j5vEqW8CWYWfB9T7v7YK_o5fzR_LPi6LNx8U31YXxxPWHx_VBUQ6nmpJM");
+messaging.usePublicVapidKey("");
 
 export {myFirebase, messaging, perf, storage};
 
