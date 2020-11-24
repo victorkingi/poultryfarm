19842e90210dcb8411e76ce8e4b03e9172359275
victorkingi <kingivictor77@gmail.com>
2020-11-24 12:14:08 +0000

    changes

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/5.9.4/firebase-messaging.js");
 
 firebase.initializeApp({
-    messagingSenderId: "921557433248"
+    messagingSenderId: ""
 });
 
 const messaging = firebase.messaging();
